b53d3ad7661fb583d20a39c46c0c8941219ce56c
debgmail <32650016+debgmail@users.noreply.github.com>
2017-11-03 18:34:59 +0530

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World - azura";
+        return "Hello Docker World azura";
     }
 
     public static void main(String[] args) {
